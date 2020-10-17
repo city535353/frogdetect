4e9fd305427eba55586376a14cc575a53d6c0779
city535353 <city535353@gmail.com>
2020-10-18 07:26:02 +0800

    Add files via upload

--- stop.js
+++ stop.js
@@ -28,6 +28,7 @@ let soundModelURL = 'https://city535353.github.io/frogdetect/model/model.json';
 function preload() {
 	createCanvas(320, 240);
 	draw();
+	setTimeout(function(){ location.href = "https://city535353.github.io/frogdetect/index.html"},300000);
   // Load the model
   //classifier = ml5.soundClassifier(soundModelURL,options,modelready);
 }
